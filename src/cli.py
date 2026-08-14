d5952e280bff886b9a151c3899299ffac3c44045
anghernandez <kari345hernandez@gmail.com>
2026-08-13 23:59:24 -0600

    Add Typer CLI for matrix sum

--- src/cli.py
+++ src/cli.py
@@ -1,11 +1,28 @@
 import typer
-from src.utils.json_loaders.json_loader import load_matrices
+from src.utils.json_loader import load_matrices
+from src.application import Application
+
+
 app = typer.Typer()
 
 @app.command()
 def sum(file_path: str):
     data = load_matrices(file_path)
-    print(data)
+
+    matrix_a = data["matrixA"]["data"]
+    matrix_b = data["matrixB"]["data"]
+
+    application = Application()
+    operation = application.get_operation("sum")
+
+    operation.SetMatrix(0, matrix_a)
+    operation.SetMatrix(1, matrix_b)
+
+    result = operation.Compute()
+    
+    print(result)
+
+    operation.Clear()
 
 if __name__ == "__main__":
     app()
